502cb6fedd3053216a00b3f26d2aa5ff3e0894e2
Deval Shah <deval.maker@gmail.com>
2020-12-05 23:13:02 -0500

    code working with team grasp env.

--- helper/models/gru.py
+++ helper/models/gru.py
@@ -18,16 +18,19 @@ class GRUActorCritic(nn.Module):
     self.apply(weight_init)
     self.distribution = DiagGaussianDistribution(action_dim=7)
     self.deterministic = False
+    self.log_std_init = 0.0
+    self.action_net, self.log_std = self.distribution.proba_distribution_net(
+                latent_dim=output_size, log_std_init=self.log_std_init
+            )
 
   def forward(self, x, h):
     x, h = self.gru(x, h)
     x = self.relu1(x)
     val = self.value(x)
-    dist = self.policy(x).squeeze(0)
-    
-    # dist = self.distribution(dist) ??
-
-    return Normal(dist, scale=1), val, h
+    latent_pi = self.policy(x).squeeze(0)
+    mean_actions = self.action_net(latent_pi)
+    dist = self.distribution.proba_distribution(mean_actions, self.log_std)
+    return dist, val, h
 
   def init_hidden_state(self, batchsize=1):
     return torch.zeros([1, batchsize, self.hidden_size])
